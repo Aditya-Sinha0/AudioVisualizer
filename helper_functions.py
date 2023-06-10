3d51a6b37a0e2e7a8a90eb7d3674cd319a59b9d6
Aditya Sinha <sinhaditya02@gmail.com>
2023-06-09 23:38:42 -0700

    Cleaner code i guess

--- helper_functions.py
+++ helper_functions.py
@@ -3,15 +3,36 @@ import numpy as np
 
 #Takes in the initial large array, and the number of vals wanted in the returned array.
 #Will delete extra vals off end if remainder present
-def shorten_list_to_averages(arr, num_vals_wanted):
-    initial_arr_length = len(arr)
-    shortened_list = []
-    arrVals_per_val = int(initial_arr_length/num_vals_wanted)
 
+#need to implement
+#need to update code to work without locInList
+def avg(lst: list[int], inc: int, locInList ):
+    return sum(lst[locInList:locInList+inc])/inc
+
+def shorten_list_to_averages(large_lst, num_vals_wanted):
+    initial_arr_length = len(large_lst)
+    group_size_to_avg = int(len(large_lst)/num_vals_wanted)
+    shortened_list = []
     i = 0
     #print("SHIT - ", len(arr)/arrVals_per_val)
-    while i+arrVals_per_val <= initial_arr_length:
-        shortened_list.append(sum(arr[i:i+arrVals_per_val])/arrVals_per_val)
-        i += arrVals_per_val
+    while i+group_size_to_avg <= initial_arr_length:
+        shortened_list.append(sum(large_lst[i:i+group_size_to_avg])/group_size_to_avg)
+        i += group_size_to_avg
     return shortened_list
+
+
+
+#Alternate methods, fuck around and find out
+#
+#large_lst = list(range(100))
+#inc = 5
+#METHOD 1
+# return_lst = []
+# for i in lst[0:len(lst)-inc:inc]:
+#     average = avg(lst, inc)
+#     return_lst.append(average)
+#
+#METHOD 2
+#final_lst = [avg(large_lst, inc, i) for i in large_lst[0:len(large_lst)-inc:inc]]
+
 
--- main.py
+++ main.py
@@ -2,35 +2,27 @@ import librosa
 from mutagen.mp3 import MP3
 from helper_functions import *
 
+def get_audio_vals(audio_path=None, polls_per_second=16):
+    audio = MP3(audio_path)
+    values, sample_rate = librosa.load(audio_path)
+    audio_duration = int(audio.info.length)
+    total_polls_wanted = audio_duration * polls_per_second
+    averages = shorten_list_to_averages(values, total_polls_wanted)
+    print(str(len(averages)) + " <-- actual vals in averages || expected total polls wanted ---> " + str(
+        total_polls_wanted))
+    print("averaged values - " + str(averages[0:10]))
+    return averages
 
-audio_path = r"C:\Users\adity\Downloads\liftmefromtheground.mp3"
-audio = MP3(audio_path)
-values, sample_rate = librosa.load(audio_path)
-
-
-audio_duration = int(audio.info.length)
-total_samples = len(values)
-#default polling rate is every 0.25 secs. add variable input later. make sure to account for more polls than samples
-polls_per_second = 4
-total_polls_wanted = audio_duration*polls_per_second
-samples_per_poll = int(total_samples/total_polls_wanted)
-
-
-
-#averages = [sum(group) / len(group) for group in zip(*[iter(values)] * samples_per_poll)]
-averages = shorten_list_to_averages(values, total_polls_wanted)
-print(str(len(averages)) + " <-- actual vals in averages || expected total polls wanted ---> " + str(total_polls_wanted))
-print("averaged values - " + str(averages[0:10]))
-
-x = np.arange(len(averages))
-plt.plot(x, averages)
-plt.xlabel('Index')
-plt.ylabel('Value')
-plt.title('Array Visualization')
-plt.show()
+def plot(lst_vals: list):
+    x = np.arange(len(lst_vals))
+    plt.plot(x, lst_vals)
+    plt.xlabel('Index')
+    plt.ylabel('Value')
+    plt.title('Array Visualization')
+    plt.show()
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print('PyCharm is running')
-
+    averages = get_audio_vals(r"C:\Users\adity\Downloads\liftmefromtheground.mp3")
+    plot(averages)
